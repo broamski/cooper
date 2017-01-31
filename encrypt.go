f05fdbdb25b37a39e8cfcce3b20724ddf48a1859
broamski <brian.nuszkowski@me.com>
2017-01-31 01:45:20 -0500

    clean up readability of encrypt keys function

--- encrypt.go
+++ encrypt.go
@@ -8,17 +8,19 @@ import (
 	"github.com/aws/aws-sdk-go/service/kms"
 )
 
+// EncryptKeys takes a kms key id, kms service, and string, turning
+// it into a base64 encoded string to provide to the portal
 func EncryptKeys(k *kms.KMS, keyid string, payload string) {
 	params := &kms.EncryptInput{
-		KeyId:     aws.String(keyid), // Required
-		Plaintext: []byte(payload),   // Required
+		KeyId:     aws.String(keyid),
+		Plaintext: []byte(payload),
 	}
 	resp, err := k.Encrypt(params)
 	if err != nil {
-		fmt.Println(fmt.Sprintf("there was an error:", err))
-        return
+		fmt.Println("there was an error:", err)
+		return
 	}
-	zz := base64.StdEncoding.EncodeToString(resp.CiphertextBlob)
+	ep := base64.StdEncoding.EncodeToString(resp.CiphertextBlob)
 	fmt.Print("here is your encrypted payload:\n")
-	fmt.Println(zz)
+	fmt.Println(ep)
 }
